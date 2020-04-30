199145810a21c47df683679e2a398d494810205a
Sarath Konuru <sarath.konuru@hitachiconsulting.com>
2020-04-30 12:21:55 +0530

    showing html in the server response

--- routes.ts
+++ routes.ts
@@ -1,4 +1,5 @@
 import * as express from 'express';
+import * as path from 'path';
 
 export class Routes {
 
@@ -6,12 +7,18 @@ export class Routes {
 
     constructor(app: express.Application) {
         this.app = app;
+        this.setStaticDir();
     }
 
     private home(): void {
         this.app.get('/', (req, res) => {
-            res.send("hey man>> wassup?");
+            //res.send("hey man>> wassup?");
+            res.sendFile('index.html');
         })
+    }
+
+    private setStaticDir() {
+        this.app.use(express.static(path.join(__dirname, '../vc')))
     }
 
     public getRoutes(): void {
