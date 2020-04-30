86c5a552d7f11530887c91917c305933525af4a6
Sarath Konuru <sarath.konuru@hitachiconsulting.com>
2020-04-30 21:04:59 +0530

    added socket.js and added socket in chat-server

--- chat-server.ts
+++ chat-server.ts
@@ -1,21 +1,25 @@
 import { createServer, Server} from 'http';
 import * as express from 'express';
+import * as socket from 'socket.io';
 
 export class ChatServer {
     public static readonly PORT:number = 5000;
     private app: express.Application;
     private port: string | number;
     private server: Server;
+    private io: SocketIO.Server;
 
     constructor() {
         this.createApp();
         this.config();
         this.createServer();
+        this.sockets();
         this.listen();
     }
 
     private createApp(): void {
         this.app = express();
+        this.app.use(express.static('vc'));
     }
 
     private config(): void {
@@ -26,10 +30,19 @@ export class ChatServer {
         this.server.listen(this.port, () => {
             console.log("Running server on port %s", this.port);
         });
+        this.io.on('connection', (socket) => {
+            socket.broadcast.emit('add-users', {
+                users: [socket.id]
+            })
+        })
     }
 
     private createServer(): void {
         this.server = createServer(this.app);
+    }
+
+    private sockets(): void {
+        this.io = socket(this.server);
     }
 
     public getApp(): express.Application {
